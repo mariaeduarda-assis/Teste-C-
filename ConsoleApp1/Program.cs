b2b50864beb8ba965f13505353fb68383d3a571b
Maria Eduarda Assis <mariaeduarda.assis16@gmail.com>
2021-04-15 22:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            int idade;
+            double salario;
+            string nome;
+
+            idade = 19;
+            salario = 1200;
+            nome = "Maria Eduarda";
+
+            //Comentários no Código
+            /* Para cógigo inteiro, sem precisar ir de linha em linha
+            if (idade > 18)
+            {
+                Console.WriteLine("Entra na festa");      
+            }
+            else
+            {
+                Console.WriteLine("Não entra na festa");
+            }
+            */
+
+
+            string valorLido;
+
+            Console.WriteLine("Informar seu nome");
+
+            valorLido = Console.ReadLine();
+
+            Console.WriteLine("Nome Lido:");
+        }
+    }
+}
